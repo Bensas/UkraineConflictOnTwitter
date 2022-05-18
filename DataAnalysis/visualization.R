fe77a40f588d0e60a6e0457791583a8f4b05c8eb
Nikita Sokovnin <sokovnik@fel.cvut.cz>
2022-05-18 22:28:34 +0900

    Add visualization style variables

--- DataAnalysis/visualization.R
+++ DataAnalysis/visualization.R
@@ -3,6 +3,9 @@ library(ggplot2)
 library(dplyr)
 
 label_colours = c('#d9534f', '#f0ad4e', '#5cb85c')
+title_size = 15
+legend_title_size = 15
+legend_text_size = 15
 
 plot_piechart <- function(data, title) {
   sentiment_by_label <- data %>%
@@ -17,10 +20,10 @@ plot_piechart <- function(data, title) {
     coord_polar("y", start=0) +
     ggtitle(title) +
     theme_void() + 
-    theme(plot.title = element_text(hjust = 0.5, size=15, face='bold', margin = margin(t = 10, r = 0 , b = 10, l = 0))) +
+    theme(plot.title = element_text(hjust = 0.5, size=title_size, face='bold', margin = margin(t = 10, r = 0 , b = 10, l = 0))) +
     #theme(axis.text.x = element_text(size=13)) +
-    theme(legend.title = element_text(face='bold', size=15)) +
-    theme(legend.text = element_text(size=15)) +
+    theme(legend.title = element_text(face='bold', size=legend_title_size)) +
+    theme(legend.text = element_text(size=legend_text_size)) +
     #theme(legend.position="none") +
     #geom_text(aes(y = ypos, label = label), color = "white", size=6) +
     scale_fill_manual('label', values=label_colours)
