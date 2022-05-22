866c09a60052a9548809cb46c4fcb8dba3eb4294
Sheikh <sheikhshafayat2@gmail.com>
2022-05-22 19:33:52 +0900

    updated my R file

--- DataAnalysis/Q3/Q3RAnalysisSheikh.R
+++ DataAnalysis/Q3/Q3RAnalysisSheikh.R
@@ -14,42 +14,77 @@ nytimes <- read.csv("/Volumes/GoogleDrive/My Drive/Spring 2022/Data Science Meth
 #######3#preprocess everything
 
 nytimes <- nytimes %>% select(Date, text, label, score)
-nytimes$Date <- substr(nytimes$Date, 0, 10) %>% as.Date("%Y-%m-%d")
+nytimes$Date <- sub(" .*", "", nytimes$Date) %>% as.Date(format="%Y-%m-%d", tz="UTC")
 nytimes$text <- tolower(nytimes$text)
 
 foxnews <- foxnews %>% select(Date, text, label, score)
-foxnews$Date <- substr(foxnews$Date, 0, 10) %>% as.Date("%Y-%m-%d")
+foxnews$Date <- sub(" .*", "", foxnews$Date) %>% as.Date(format="%Y-%m-%d", tz="UTC")
 foxnews$text <- tolower(foxnews$text)
-#########Copied these codes from Juan
-# convert string to datetime
-q1$Date <- sub(" .*", "", q1$Date)
-q1$Date <- as.Date(q1$Date, format="%Y-%m-%d", tz="UTC")
+
+#########Copied these codes from Juan's code##########
+
 
 # create YearMonth column
-q1$YearMonth <- substr(q1$Date, 1,7)
-
+foxnews$YearMonth <- substr(foxnews$Date, 1,7)
+nytimes$YearMonth <- substr(nytimes$Date, 1,7)
 # create Week column
-q1 <- q1 %>% 
-  mutate(Week = cut.Date(q1$Date, breaks = "1 week", labels = FALSE)) %>% 
-  arrange(q1$Date)
+foxnews <- foxnews %>% 
+  mutate(Week = cut.Date(foxnews$Date, breaks = "1 week", labels = FALSE)) %>% 
+  arrange(foxnews$Date)
+
+nytimes <- nytimes %>% 
+  mutate(Week = cut.Date(nytimes$Date, breaks = "1 week", labels = FALSE)) %>% 
+  arrange(nytimes$Date)
 
 # calculate percentage of sentiment by week
-sentiment_by_week <- q1 %>%
+sentiment_by_week_fox <- foxnews  %>%
   group_by(Week, label) %>%
   summarise(cnt = n()) %>%
   mutate(freq = round(cnt / sum(cnt), 3)) %>% 
-  arrange(Week)
+  arrange(Week) %>% mutate(source = "FoxNews")
+
+sentiment_by_week_nytimes <- nytimes %>%
+  group_by(Week, label) %>%
+  summarise(cnt = n()) %>%
+  mutate(freq = round(cnt / sum(cnt), 3)) %>% 
+  arrange(Week) %>% mutate(source = "NYTimes")
+
+sentiment_by_week_combined <- rbind(sentiment_by_week_fox, sentiment_by_week_nytimes)
 
 # calculate percentage of sentiment by month
-sentiment_by_month <- q1 %>%
+sentiment_by_month_fox <- foxnews %>%
   group_by(YearMonth, label) %>%
   summarise(cnt = n()) %>%
   mutate(freq = round(cnt / sum(cnt), 3)) %>% 
-  arrange(YearMonth)
+  arrange(YearMonth) %>% mutate(source = "FoxNews")
+
+sentiment_by_month_nytimes <- nytimes %>%
+  group_by(YearMonth, label) %>%
+  summarise(cnt = n()) %>%
+  mutate(freq = round(cnt / sum(cnt), 3)) %>% 
+  arrange(YearMonth) %>% mutate(source = "NYTimes")
+
+sentiment_by_month_combined <- rbind(sentiment_by_month_fox, sentiment_by_month_nytimes)
+
+###Plotting graphs
+ggplot(sentiment_by_week_combined, aes(fill=source, y=freq, x=Week)) + 
+  geom_bar(position='dodge', stat='identity') +
+  theme_minimal() + 
+  theme(panel.background = element_blank()) +
+  ggtitle("Average Sentiment of Tweets by Week") +
+  labs(x='Week', y='Frequency') +
+  theme(plot.title = element_text(hjust = 0.5, size=20, face='bold', margin = margin(t = 10, r = 0 , b = 10, l = 0))) +
+  theme(axis.title.x = element_text(face='bold', size=15, margin = margin(t = 10, b = 10, r = 0, l = 0))) +
+  theme(axis.title.y = element_text(face='bold', size=15, margin = margin(t = 0, b = 0, r = 10, l = 10))) +
+  theme(axis.text.x = element_text(angle=30, size=13)) +
+  theme(axis.text.y = element_text(size=13)) +
+  theme(legend.title = element_text(face='bold', size=15)) +
+  theme(legend.text = element_text(size=15)) +
+  scale_x_continuous(breaks=c(1,11,16,23), labels=c("Dec 24th", "Feb 24th", "April 7th", "May 24th")) +
+  scale_fill_manual('label', values=c('#d9534f', '#f0ad4e', '#5cb85c'))
 
 
-
-####Checking the overall positivity of the news
+####Checking the overall sentiment of the news
 foxTotal <- table(foxnews$label)/length(foxnews$label) * 100
 nyTotal <- table(nytimes$label)/length(nytimes$label) * 100
 
