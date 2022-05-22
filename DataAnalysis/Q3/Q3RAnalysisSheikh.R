1de40bfbe171a4c9130dd3bc7d8c34939142d1b6
Sheikh <sheikhshafayat2@gmail.com>
2022-05-22 19:33:52 +0900

    filtered data for war tweets and made some visualizations in the R file

--- DataAnalysis/Q3/Q3RAnalysisSheikh.R
+++ DataAnalysis/Q3/Q3RAnalysisSheikh.R
@@ -6,18 +6,20 @@ p_load(ggplot2)
 p_load(dplyr)
 p_load(reshape2)
 p_load(gridExtra)
+p_load(stringr)
 #reading data
 foxnews <- read.csv("/Volumes/GoogleDrive/My Drive/Spring 2022/Data Science Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q3/FoxNews_Sheikh_with_sentiment.csv")
 nytimes <- read.csv("/Volumes/GoogleDrive/My Drive/Spring 2022/Data Science Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q3/NYT_Sheikh_with_sentiment.csv")
 #preprocess everything
-foxnews <- foxnews %>% select(Date, text, label, score)
 
 nytimes <- nytimes %>% select(Date, text, label, score)
-
-
-
 nytimes$Date <- substr(nytimes$Date, 0, 10) %>% as.Date("%Y-%m-%d")
+nytimes$text <- tolower(nytimes$text)
+
+foxnews <- foxnews %>% select(Date, text, label, score)
 foxnews$Date <- substr(foxnews$Date, 0, 10) %>% as.Date("%Y-%m-%d")
+foxnews$text <- tolower(foxnews$text)
+
 
 foxTotal <- table(foxnews$label)/length(foxnews$label) * 100
 nyTotal <- table(nytimes$label)/length(nytimes$label) * 100
@@ -50,6 +52,8 @@ gnyt2 <- ggplot(nyTotal, aes(x=Sentiment, y=Percentage, fill=Sentiment)) +
   theme(legend.position="none", panel.background = element_blank()) 
 
 #Plots side by side in one graph
+#This is the side by side comparison of the sentiment of tweets of all tweets
+
 ggplot(allSent, aes(x=Sentiment, y=Percentage, fill=Source)) +
   geom_bar(stat="identity", width = 1, position = position_dodge()) +
   theme(panel.background = element_blank()) +
@@ -57,4 +61,32 @@ ggplot(allSent, aes(x=Sentiment, y=Percentage, fill=Source)) +
 
 
 
+############## Filtering out war related tweets####################
+#we look for the following words
+warwords <- c("invasion", "ukraine", "russia", "war", "putin", "zelensky", "nato", "eu", "invade")
 
+warTweet <- function(text) {
+  word = unlist(strsplit(text, " "))[1]
+  if (word %in% warwords) {
+    TRUE
+  } else{
+    FALSE
+  } 
+}
+warfox <- foxnews %>% filter(sapply(foxnews$text, warTweet))
+warnyt <- nytimes %>% filter(sapply(nytimes$text, warTweet))
+
+######Plotting similar graph for war tweets:
+foxTotal2 <- table(warfox$label)/length(warfox$label) * 100
+nyTotal2 <- table(warnyt$label)/length(warnyt$label) * 100
+#positive and negative newstitles
+foxTotal2 <- melt(foxTotal2) %>% rename(c(Sentiment = Var1, Percentage=value)) %>% mutate(Source="FoxNews")
+nyTotal2 <- melt(nyTotal2) %>% rename(c(Sentiment = Var1, Percentage=value)) %>% mutate(Source="NYT")
+allSent2 <- rbind(foxTotal2, nyTotal2)
+ggplot(allSent2, aes(x=Sentiment, y=Percentage, fill=Source)) +
+  geom_bar(stat="identity", width = 1, position = position_dodge()) +
+  theme(panel.background = element_blank()) +
+  scale_fill_manual(values=c("#fc4949", "#1a94eb"))
+
+dim(warfox)
+dim(warnyt)
