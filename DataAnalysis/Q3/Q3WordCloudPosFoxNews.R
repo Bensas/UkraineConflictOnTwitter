30bc1dc0f960c1d1e03a6e8e50d67c64b0deaa02
Sheikh <sheikhshafayat2@gmail.com>
2022-05-22 19:33:52 +0900

    made the word clouds, however, needs refinements

---
+++ DataAnalysis/Q3/Q3WordCloudPosFoxNews.R
@@ -0,0 +1,31 @@
+# Q1 wordcloud
+
+# source file -- change the file path here
+fox_cloud <- read.csv("/Volumes/GoogleDrive/My Drive/Spring 2022/Data Science Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q3/fox_news_Final_with_sentiment.csv")
+
+positive <- fox_cloud[fox_cloud$label == 'Neutral',] 
+
+# remove non-ascii words
+positive$text <- stringi::stri_trans_general(positive$text, "latin-ascii")
+positive$text <- gsub("[^\x01-\x7F]", "", positive$text)
+
+# create corpus and preprocess data
+docs <- Corpus(VectorSource(positive$text))
+docs <- docs %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace)
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removeWords, c("russia", "ukraine", "user")) # remove "Russia" and "Ukraine"
+
+# create matrix
+dtm <- TermDocumentMatrix(docs) 
+matrix <- as.matrix(dtm) 
+words <- sort(rowSums(matrix),decreasing=TRUE) 
+df <- data.frame(word = names(words),freq=words)
+
+# create wordcloud
+set.seed(1234)
+wordcloud(words = df$word, freq = df$freq, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
