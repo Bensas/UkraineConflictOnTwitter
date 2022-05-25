20d1c5c56712d3488e1fd62c7e19d317b017db79
Bensas <juanbensa@gmail.com>
2022-05-25 12:57:32 +0900

    Added Q2 piecharts

--- DataAnalysis/Q2/q2_piecharts.R
+++ DataAnalysis/Q2/q2_piecharts.R
@@ -1,14 +1,24 @@
 # Q2 analysis
 
 # we should unify paths (this path works if your working directory contains the repository)
-source("UkraineConflictOnTwitter/DataAnalysis/visualization.R")
+source("/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/DataAnalysis/visualization.R")
 
 # csv file -- change the file path here
-q2_russian_putin <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_russian_with_sentiment.csv')
-q2_russian_zelensky <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_russian_with_sentiment.csv')
-#q2_russian_nato <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_russian_with_sentiment.csv')
+q2_russian_putin <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_russian_with_sentiment.csv')
+q2_russian_zelensky <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_russian_with_sentiment.csv')
+q2_russian_nato <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_russian_with_sentiment.csv')
+
+q2_english_putin <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_english_with_sentiment.csv')
+q2_english_zelensky <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_english_with_sentiment.csv')
+q2_english_nato <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_english_with_sentiment.csv')
 
 plot_piechart(q2_russian_putin, "Sentiment of Russian tweets containing 'Putin'")
+plot_piechart(q2_english_putin, "Sentiment of English tweets containing 'Putin'")
+
 plot_piechart(q2_russian_zelensky, "Sentiment of Russian tweets containing 'Zelensky'")
+plot_piechart(q2_english_zelensky, "Sentiment of English tweets containing 'Zelensky'")
+
+plot_piechart(q2_russian_nato, "Sentiment of Russian tweets containing 'NATO'")
+plot_piechart(q2_english_nato, "Sentiment of English tweets containing 'NATO'")
 
 
