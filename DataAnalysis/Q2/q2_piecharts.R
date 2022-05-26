5bffd7490c87ece1e4d8445e2eb9e06382950157
Nikita Sokovnin <sokovnik@fel.cvut.cz>
2022-05-26 10:34:04 +0900

    Add another model for Russian sentiment results

--- DataAnalysis/Q2/q2_piecharts.R
+++ DataAnalysis/Q2/q2_piecharts.R
@@ -1,16 +1,16 @@
 # Q2 analysis
 
 # we should unify paths (this path works if your working directory contains the repository)
-source("/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/DataAnalysis/visualization.R")
+source("UkraineConflictOnTwitter/DataAnalysis/visualization.R")
 
 # csv file -- change the file path here
-q2_russian_putin <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_russian_with_sentiment.csv')
-q2_russian_zelensky <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_russian_with_sentiment.csv')
-q2_russian_nato <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_russian_with_sentiment.csv')
+q2_russian_putin <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_russian_with_sentiment.csv')
+q2_russian_zelensky <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_russian_with_sentiment.csv')
+q2_russian_nato <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_russian_with_sentiment.csv')
 
-q2_english_putin <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_english_with_sentiment.csv')
-q2_english_zelensky <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_english_with_sentiment.csv')
-q2_english_nato <-  read.csv('/Users/Bensas/ITBA/Intercambios/KAIST/Data\ Science\ Methodology/UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_english_with_sentiment.csv')
+q2_english_putin <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/putin_english_with_sentiment.csv')
+q2_english_zelensky <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/zelensky_english_with_sentiment.csv')
+q2_english_nato <-  read.csv('UkraineConflictOnTwitter/SentimentAnalysis/data/q2/nato_english_with_sentiment.csv')
 
 plot_piechart(q2_russian_putin, "Sentiment of Russian tweets containing 'Putin'")
 plot_piechart(q2_english_putin, "Sentiment of English tweets containing 'Putin'")
